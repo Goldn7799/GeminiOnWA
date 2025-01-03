78908cbd623b006ce6e637157728ea9fc4b0102d
Goldn7799 <syeifsultan@gmail.com>
2025-01-03 13:52:06 +0700

    Fix Error

--- plugins/generalAI.ts
+++ plugins/generalAI.ts
@@ -18,50 +18,57 @@ import { prompt } from "../modules/Gemini";
 import smallDB from "../modules/WhatsApp/smallDB";
 import parser from "../modules/WhatsApp/parser";
 import utility from "../modules/WhatsApp/utility";
+import logging from "../modules/logging";
 
 /**
  * Default WhatsApp Response from AI({@link prompt})
  * @public
  */
 const generalAI = async (sock: WASocket, msg: WAMessage) => {
-  if (!msg.key.fromMe && msg.message) {
-    /**
-     * This is a Chat Session Key
-     */
-    const session = msg.key.remoteJid!
-    
-    /**
-     * Check if the chat is suitable for a response 
-     */
-    if (session.includes('@s.whatsapp.net') || (session.includes('@g.us') && msg.message?.extendedTextMessage?.contextInfo?.mentionedJid?.includes(`${sock.user?.id.split(':')[0]}@s.whatsapp.net`))) {
-      const thisHistory = (smallDB.check(session)) ? smallDB.get(session) : null
-      const thisMessageText = (msg.message?.conversation) ? msg.message.conversation : msg.message.extendedTextMessage?.text
-      const replyMessage = msg.message.extendedTextMessage?.contextInfo?.quotedMessage
-      const thisMedia = (msg.message?.imageMessage || msg.message?.stickerMessage) ? await downloadMediaMessage(msg, 'buffer', {}) : null
-      // const replyMessageMedia = (replyMessage?.imageMessage || replyMessage?.stickerMessage) ? await downloadMediaMessage(messageParser(sock, msg), 'buffer', {}) : null
-
+  try {
+    if (!msg.key.fromMe && msg.message) {
       /**
-       * Indicator if chat being responded 
+       * This is a Chat Session Key
        */
-      await sock.readMessages([msg.key])
-      await sock.sendPresenceUpdate('composing', session)
-
+      const session = msg.key.remoteJid!
+      
       /**
-       * Choose wich one AI Prompt to be use 
+       * Check if the chat is suitable for a response 
        */
-      const aiRes = (thisMedia)
-      ? await prompt.charImagePrompt(`${(replyMessage?.conversation) ? `([Reply From Message] "${replyMessage?.conversation}")` : ''}${thisMessageText}`, [utility.toArrayBuffer(thisMedia)], thisHistory)
-      : await prompt.charTextPrompt(`${(replyMessage?.conversation) ? `([Reply From Message] "${replyMessage?.conversation}")` : ''}${thisMessageText}`, thisHistory)
-
-      /**
-       * If response Avaiable, send it to user 
-       */
-      if (aiRes) {
-        smallDB.update(session, aiRes?.history)
-        await sock.sendMessage(session, { text: parser(`${aiRes?.result}`) }, { quoted: msg })
-        await sock.sendPresenceUpdate('available', session)
-      }
-    };
+      if (session.includes('@s.whatsapp.net') || (session.includes('@g.us') && msg.message?.extendedTextMessage?.contextInfo?.mentionedJid?.includes(`${sock.user?.id.split(':')[0]}@s.whatsapp.net`))) {
+        /**
+         * Indicator if chat being responded 
+         */
+        await sock.readMessages([msg.key])
+        await sock.sendPresenceUpdate('composing', session)
+        
+        const thisHistory = (smallDB.check(session)) ? smallDB.get(session) : null
+        const thisMessageText = (msg.message?.conversation) ? msg.message.conversation : msg.message.extendedTextMessage?.text
+        const replyMessage = msg.message.extendedTextMessage?.contextInfo?.quotedMessage
+        const thisMedia = (msg.message?.imageMessage || msg.message?.stickerMessage) ? await downloadMediaMessage(msg, 'buffer', {}) : null
+        // const replyMessageMedia = (replyMessage?.imageMessage || replyMessage?.stickerMessage) ? await downloadMediaMessage(messageParser(sock, msg), 'buffer', {}) : null
+  
+        /**
+         * Choose wich one AI Prompt to be use 
+         */
+        const aiRes = (thisMedia)
+        ? await prompt.charImagePrompt(`${(replyMessage?.conversation) ? `([Reply From Message] "${replyMessage?.conversation}")` : ''}${thisMessageText}`, [utility.toArrayBuffer(thisMedia)], thisHistory)
+        : await prompt.charTextPrompt(`${(replyMessage?.conversation) ? `([Reply From Message] "${replyMessage?.conversation}")` : ''}${thisMessageText}`, thisHistory)
+  
+        /**
+         * If response Avaiable, send it to user 
+         */
+        if (aiRes) {
+          smallDB.update(session, aiRes?.history)
+          await sock.sendMessage(session, { text: parser(`${aiRes?.result}`) }, { quoted: msg })
+          await sock.sendPresenceUpdate('available', session)
+        }
+      };
+    }
+  } catch (err) {
+    await sock.sendMessage(msg.key.remoteJid!, { text: "Yah Media nya gak bisa ke download :<" }, { quoted: msg })
+    await sock.sendPresenceUpdate('available', msg.key.remoteJid!)
+    logging.add(`${err}`)
   }
 }
 
