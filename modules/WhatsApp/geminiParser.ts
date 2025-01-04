2f338fa99f6568df0fb476ace319311a9bd00b6d
Goldn7799 <syeifsultan@gmail.com>
2025-01-04 15:08:24 +0700

    benerin char nya

--- modules/WhatsApp/geminiParser.ts
+++ modules/WhatsApp/geminiParser.ts
@@ -18,8 +18,8 @@
  * @public
  */
 const geminiParser = (text: string) => {
-  let parsedText: string = text.replaceAll(/\*\*(.*)\*\*/g, "*$1*")
-  parsedText = parsedText.replaceAll(/\* (.*)/g, "- $1")
+  const parsedText: string = text.replaceAll(/\*\*(.*)\*\*/g, "*$1*")
+  // parsedText = parsedText.replaceAll(/\* (.*)/g, "- $1")
   return parsedText
 }
 
